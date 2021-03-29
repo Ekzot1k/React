29b94a3eaa8f798cfb5bc0ed8f896e53b1862d10
Denis <Norm1k@yandex.ru>
2021-03-29 22:58:28 +0500

    Work_9

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+import Technologies from './Techologies.js';
+import Header from './Header.js';
 
 function App() {
   return (
     <div>
-      <div>
-        <h1>DIV_1 App</h1>
-      </div>
-      <div>
-        <h1>DIV_2 App</h1>
-      </div>
+      <Technologies/>
+      <Header/>
     </div>
   );
 }
