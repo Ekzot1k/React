cfe2bc5af82cbe879c2e57da9648f060d7feebd4
Denis <Norm1k@yandex.ru>
2021-03-30 08:49:56 +0500

    Work_12
    Создал фалы под компоненты

---
+++ src/Components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return <header className="header">
+        <img src='https://w7.pngwing.com/pngs/747/118/png-transparent-lion-logo-roar-lions-lion-y-white-mammal-face.png'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://fsd.multiurok.ru/html/2017/06/25/s_594fba42bf816/653571_1.jpeg'></img>
+        </div>
+        <div>
+            Avatar + Description
+    </div>
+        <div>
+            My post
+      <div>
+                New post
+      </div>
+            <div>
+                post 1
+      </div>
+            <div>
+                post 2
+      </div>
+        </div>
+    </div>
+}
+
+export default Profile;
