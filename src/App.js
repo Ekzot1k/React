34f77eaf4dca5a7e5a6ae6dc088e66631f1a97fa
Denis <Norm1k@yandex.ru>
2021-03-30 08:25:33 +0500

    Work_11
    Добавил Grid
    Создал разметку
    Создал шаблон страницы

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import Header from "./Header";
-import Technologies from "./Techologies";
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Technologies/>
-      <Header/>
+    <div className="app-Wrapper">
+      <header className="header">
+        <img src='https://w7.pngwing.com/pngs/747/118/png-transparent-lion-logo-roar-lions-lion-y-white-mammal-face.png'></img>
+      </header>
+      <nav className='nav'>
+        <div>
+          <a href="#">Profile</a>
+        </div>
+        <div>
+          <a href="#">Message</a>
+        </div>
+        <div>
+          <a href="#">News</a>
+        </div>
+        <div>
+          <a href="#">Music</a>
+        </div>
+        <div>
+          <a href="#">Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://fsd.multiurok.ru/html/2017/06/25/s_594fba42bf816/653571_1.jpeg'></img>
+        </div>
+        <div>
+          Avatar + Description
+        </div>
+        <div>
+          My post
+          <div>
+            New post
+          </div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
